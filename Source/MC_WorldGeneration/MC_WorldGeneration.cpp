e16a27f9f3b5b6cedeb504e9f922274a317c5745
leonctl <leonchantinglok@gmail.com>
2022-12-13 10:28:54 +0800

    Create Project

---
+++ Source/MC_WorldGeneration/MC_WorldGeneration.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "MC_WorldGeneration.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MC_WorldGeneration, "MC_WorldGeneration" );
---
+++ Source/MC_WorldGeneration/MC_WorldGenerationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MC_WorldGenerationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MC_WORLDGENERATION_API AMC_WorldGenerationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
