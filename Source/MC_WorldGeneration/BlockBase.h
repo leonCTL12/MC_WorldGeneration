5ac54abb88981762f8defb6ff136d875f8cc9d11
leonctl <leonchantinglok@gmail.com>
2022-12-13 11:42:52 +0800

    Created BlockBase Class

---
+++ Source/MC_WorldGeneration/BlockBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlockBase.generated.h"
+
+UCLASS()
+class MC_WORLDGENERATION_API ABlockBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABlockBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
