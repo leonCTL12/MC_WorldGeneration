2a9f643e937eb09b5ffcc2288f95d55123198e28
leonctl <leonchantinglok@gmail.com>
2022-12-15 13:51:03 +0800

    added a crosshair to the character

--- Source/MC_WorldGeneration/MC_WorldGeneration.Build.cs
+++ Source/MC_WorldGeneration/MC_WorldGeneration.Build.cs
@@ -8,6 +8,6 @@ public class MC_WorldGeneration : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG" });
 	}
 }
